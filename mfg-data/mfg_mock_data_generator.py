722192fa83ce77f42abe7196cc99c118495d6663
John Davis <64516850+whidbeyisland@users.noreply.github.com>
2022-11-10 21:35:13 -0800

    Adding mock data generator support for datetime and string columns

--- mfg-data/mfg_mock_data_generator.py
+++ mfg-data/mfg_mock_data_generator.py
@@ -1,11 +1,11 @@
-import pandas as pd
 import numpy as np
+import pandas as pd
 import random
+import uuid
 
 from mfg_data_formats import fmt_00001_columns
 
-df = pd.read_csv("continuous_factory_process.csv")
-
+# df = pd.read_csv("continuous_factory_process.csv")
 # for col in df.columns:
 #     try:
 #         print("'%s': [%8.8f, %8.8f]," % (col, df[col].min(), df[col].max()))
@@ -19,10 +19,27 @@ df = pd.DataFrame(columns = columns)
 for i in range(0, 5):
     row = []
     for col in columns:
-        min = fmt_00001_columns[col][0]
-        max = fmt_00001_columns[col][1]
-        val = round(random.uniform(min, max), 8)
-        row.append(val)
+        # fill the column with an appropriate random value depending on its type, e.g. "number", "datetime"
+        if fmt_00001_columns[col][0] == 'number':
+            min = fmt_00001_columns[col][1]
+            max = fmt_00001_columns[col][2]
+            val = round(random.uniform(min, max), 8)
+            row.append(val)
+        
+        elif fmt_00001_columns[col][0] == 'datetime':
+            year = random.randint(1970, 2069)
+            month = random.randint(1, 12)
+            day = random.randint(1, 28)
+            hour = random.randint(0, 23)
+            minute = random.randint(0, 59)
+            second = random.randint(0, 59)
+            val = '%04d-%02d-%02d %02d:%02d:%02d' % (year, month, day, hour, minute, second)
+            row.append(val)
+        
+        elif fmt_00001_columns[col][0] == 'string':
+            val = uuid.uuid4()
+            row.append(val)
+
     row_df = pd.DataFrame([row], columns=columns)
     df = pd.concat([df, row_df], ignore_index=True)
 df.to_csv('test.csv', sep=',', encoding='utf-8')
